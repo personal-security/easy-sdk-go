ed43a3f7555cb8c0721127f0716e1cd39fa901e8
personal-security <maxrus13@icloud.com>
2021-08-18 10:51:31 +0300

    add check slice string

--- utils_array.go
+++ utils_array.go
@@ -20,3 +20,13 @@ func ReverseFloat64(input []float64) []float64 {
 	}
 	return append(ReverseFloat64(input[1:]), input[0])
 }
+
+func SliceHaveString(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
